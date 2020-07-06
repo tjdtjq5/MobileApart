db0f503a788741e53eb38efeddbea9f3ad6634d2
tjdtjq5 <tjdtjq5@naver.com>
2020-07-06 20:24:01 +0900

    98489

---
+++ Assets/Script/상점/Shop.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public GameObject shopPannel;
+
+    public void ShopOpen()
+    {
+        shopPannel.gameObject.SetActive(true);
+    }
+
+    public void ShopClose()
+    {
+        shopPannel.gameObject.SetActive(false);
+    }
+}
